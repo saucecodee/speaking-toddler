3ba58521f83e7db1cc925e9348eb262655a67dac
saucecodee <ashawinta@gmail.com>
2020-07-22 15:19:01 +0100

    A lilule update

---
+++ form.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+$db = mysqli_connect('localhost', 'id14361159_mide', 'dBCEz-)*Xc{X1|~K', 'id14361159_root');
+
+// Check connection
+if (!$db) {
+     die("Connection failed: " . mysqli_connect_error());
+}
+
+// only process the form if $_POST is not empty
+if (isset($_POST['submit'])) {
+     $email = trim($_POST["email"]);
+
+     // Insert our data
+     $sql = "INSERT INTO users (email) VALUES ( '$email')";
+     $insert = mysqli_query($db, $sql);
+
+     // Print response from MySQL
+     if ($insert) {
+          $_SESSION["message"] = "Thanks for signing up !";
+          header("location: index.php");
+     } else {
+          $_SESSION["message"] = "Sorry, please try again";
+          header("location: index.php");
+     }
+
+}
---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     <link rel="stylesheet" href="./styles/sauce.css">
+     <title>Speaking toddler</title>
+</head>
+
+<body>
+     <!--==================== header ==================== -->
+
+     <header>
+          <div class="container">
+               <div class="brand">The speaking toddler</div>
+
+
+               <nav>
+                    <span>About</span>
+                    <span>Features</span>
+                    <span>Benefits</span>
+                    <span>Features</span>
+               </nav>
+          </div>
+     </header>
+
+
+     <!--==================== Hero ==================== -->
+
+     <div id="hero">
+          <div class="container">
+               <h1 class="title">Baby cry translated to plain english</h1>
+
+               <div class="count-down">
+                    <div class="container">
+
+                         <div>
+                              <div class="num">20</div>
+                              <p>Days</p>
+                         </div>
+                         <div>
+                              <div class="num">20</div>
+                              <p>Hours</p>
+                         </div>
+                         <div>
+                              <div class="num">20</div>
+                              <p>Minutes</p>
+                         </div>
+                         <div>
+                              <div class="num">20</div>
+                              <p>Seconds</p>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+
+
+     <!--==================== About  ==================== -->
+
+     <div id="about">
+          <div class="container">
+               <div class="content">
+                    <h2 class="sec-title">About the app</h2>
+                    <p>“The Speaking Toddler” translates every single movement, cry, verbal and non verbal emotio ns of
+                         toddlers into plain English. This way par ents and guardians understands what their to ddlers
+                         are trying to communicate per time.</p>
+               </div>
+               <div class="baby-img">
+                    <img src="./assets/about/baby-1.png" alt="">
+               </div>
+          </div>
+
+          <div class="yellow-bg"></div>
+     </div>
+
+
+     <!--==================== App features  ==================== -->
+
+     <div id="app-features">
+          <h2 class="sec-title">App Features</h2>
+
+          <div class="content">
+               <div class="container">
+                         <div id="carouselExampleControls" class="carousel slide " data-ride="carousel">
+                              <div class="carousel-inner ">
+                                <div class="carousel-item active">
+                                   <div class="c-item">
+                                        <p class="text">The Speaking Toddler translates every single movement, cry, verbal and non
+                                             verbal emotions of toddlers into plain English. This way parents and guardians understands
+                                             whattheir toddlers are trying tocommunicate per time.</p>
+                                        <img src="./assets/app-features/screen-1.png" alt="">
+                                   </div>
+                                </div>
+                                <div class="carousel-item">
+                                   <div class="c-item">
+                                        <p class="text">Sign up with us in order to benefit from all the amazing features our app offers.</p>
+                                        <img src="./assets/app-features/screen-2.svg" alt="">
+                                   </div>
+                                </div>
+                                <div class="carousel-item">
+                                   <div class="c-item">
+                                        <p class="text">We have developed technology that helps listen to your baby cry and converts or translate the cry immediately into plain english the mother can undertsand </p>
+                                        <img src="./assets/app-features/screen-3.svg" alt="">
+                                   </div>
+                                </div>
+                                <div class="carousel-item ">
+                                   <div class="c-item">
+                                        <p class="text">You can now scan your baby actions,movement and countenanace to know what the baby wants per time.</p>
+                                        <img src="./assets/app-features/screen-4.svg" alt="">
+                                   </div>
+                                </div>
+                                <div class="carousel-item ">
+                                   <div class="c-item">
+                                        <p class="text">Our app helps to detect when the baby is lonely or in a  sad mood then send a notification and suggest a song that will cheer up the  baby.</p>
+                                        <img src="./assets/app-features/screen-5.svg" alt="">
+                                   </div>
+                                </div>
+                                <div class="carousel-item ">
+                                   <div class="c-item">
+                                        <p class="text">Some baby plays with food even when they arent filled up.Our app helps to detect if a baby is fully filled up during feeding /breast-feeding.</p>
+                                        <img src="./assets/app-features/screen-6.svg" alt="">
+                                   </div>
+                                </div>
+                              </div>
+                              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                         </div>
+
+               </div>
+          </div>
+     </div>
+
+
+     <!--==================== Other Benefits  ==================== -->
+
+     <div id="benefits">
+          <h2 class="sec-title">Other benefits we offer</h2>
+
+          <div class="container">
+               <div class="benefit">
+                    <div class="num">01</div>
+                    <p class="text">Daily articles for parents on baby growth, food etc</p>
+               </div>
+               <div class="benefit">
+                    <div class="num">02</div>
+                    <p class="text">Information about baby stores around your vicinity </p>
+               </div>
+               <div class="benefit">
+                    <div class="num">03</div>
+                    <p class="text">Information about baby stores around your vicinity </p>
+               </div>
+               <div class="benefit">
+                    <div class="num">04</div>
+                    <p class="text">Daily articles for parents on baby growth, food etc</p>
+               </div>
+          </div>
+     </div>
+
+
+     <!--==================== Call to action  ==================== -->
+
+     <div id="parents">
+          <h2 class="sec-title">What parents are saying</h2>
+          <div class="cards-container">
+               <div class="container">
+                    <div class="card">
+
+                    </div>
+               </div>
+          </div>
+     </div>
+
+
+     <!--==================== Call to action  ==================== -->
+
+     <div id="call-t-a">
+          <div class="container">
+               <h2 class="sec-title">Be the first to know</h2>
+
+               <p class="c-text">Be the first to know about our app when it is out.Kindly
+                    input your email address below</p>
+
+               <form action="form.php" method="post">
+                    <input type="email" name="email" placeholder="Your email address" required autofocus>
+                    <button class="but-0" name="submit" type="submit">Join the waitlist</button>
+               </form>
+          </div>
+     </div>
+
+
+     <!--==================== Footer  ==================== -->
+
+     <footer>
+          <div class="container">
+               <div>The <span class="col-y">speaking</span> toddler</div>
+               <div>Designed by @Mhiday</div>
+          </div>
+     </footer>
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.0/gsap.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.0/ScrollTrigger.min.js"></script>
+     <script src="./js/global.js"></script>
+     <script src="./js/anime.js"></script>
+     <script src="./js/page.js"></script>
+</body>
+
+</html>
